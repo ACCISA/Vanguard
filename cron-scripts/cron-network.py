d2d9e2a04d8057959b4f283ac6e563b5c9e80168
ACCISA <aniss.chalah@gmail.com>
2024-09-14 22:15:34 -0400

    prome monitoring and modules created

--- cron-scripts/cron-network.py
+++ cron-scripts/cron-network.py
@@ -1,51 +1,20 @@
 import nmap3
 import json
-
 from prometheus_client import CollectorRegistry, Counter, Gauge, generate_latest
 import re
 import os
 
+from ingest import append_json
+from prometheus import increment_counter
+
+
 METRICS_FILE = '/var/log/prom/network_counter.prom'
-
-def parse_metrics_file():
-    if not os.path.exists(METRICS_FILE):
-        return 0.0  # Default values if file does not exist
-
-    with open(METRICS_FILE, 'r') as f:
-        content = f.read()
-    
-    counter_match = re.search(r'^network_counter_total\s+(\d+\.?\d*)$', content, re.MULTILINE)
-
-    current_counter = float(counter_match.group(1)) if counter_match else 0.0
-
-    return current_counter
-
-def increment_and_save_counter():
-    """Increment the counter and update the metrics file."""
-    # Parse the existing metrics file
-    current_counter = parse_metrics_file()
-    print("current counter: ",current_counter)
-
-    # Create a registry and define metrics
-    registry = CollectorRegistry()
-    REQUESTS = Gauge('network_counter_total', 'Times cron job ran', registry=registry)
-
-    # Set the counter to the current value and increment
-    REQUESTS.set(float(current_counter)+float(1))
-
-    metrics_data = generate_latest(registry).decode('utf-8')
-
-    # Write the updated metrics data to the file
-    with open(METRICS_FILE, 'w') as f:
-        f.write(metrics_data)
 
 nmap = nmap3.Nmap()
 results = nmap3.NmapScanTechniques().nmap_ping_scan("10.0.0.1-5")
-increment_and_save_counter()
+increment_counter('network_counter_total',METRICS_FILE)
 
-
-with open("/var/log/nmap_network_results.json","a") as log:
-    json.dump(results, log, indent=4)
+append_json("/var/log/json/nmap_network.json", results)
 
 
 
