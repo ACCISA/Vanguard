9f0c185832e5f2a0c27ae1f68a1f3d32506f351f
ACCISA <aniss.chalah@gmail.com>
2024-09-15 10:56:39 +0100

    update ports

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',  // Bind to all network interfaces
+    port: 3000,       // You can change this to the desired port
+  },
   define: {
     'process.env': process.env
   },
--- cron-scripts/cron-ports.py
+++ cron-scripts/cron-ports.py
@@ -1,4 +1,5 @@
 import json
+import csv
 import nmap
 import os
 
@@ -25,12 +26,17 @@ def read_up_file():
 data = read_up_file()
 nm = nmap.PortScanner()
 f = open(CSV_FILE,"a")
+idx = 0
 for machine in data:
-    nm.scan(machine['ip'],'20-500')
-    lines = nm.csv().strip('\n')
-    idx = 0
+    nm.scan(machine['ip'],'20-513')
+    lines = nm.csv().split('\n')
     for line in lines:
+        if idx == 0:
+            f.write(line+"\n")
+            idx = 1
+            continue
         if "hostname_type" in line: continue
-        f.write(line)
+        f.write(line+"\n")
 
 f.close()
+
